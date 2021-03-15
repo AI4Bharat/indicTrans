bf0d3a1acc12760bb2b468c2befefc93b242d41c
Gowtham R <gowtham.ramesh1@gmail.com>
2021-03-15 17:41:20 +0530

    fix path

--- prepare_data.sh
+++ prepare_data.sh
@@ -15,30 +15,30 @@ mkdir -p $devtest_processed_dir
 mkdir -p $out_data_dir
 
 # train preprocessing
-$train_infname_src=$train_data_dir/train.$src_lang
-$train_infname_tgt=$train_data_dir/train.$tgt_lang
-$train_outfname_src=$train_processed_dir/train.SRC
-$train_outfname_tgt=$train_processed_dir/train.TGT
+train_infname_src=$train_data_dir/train.$src_lang
+train_infname_tgt=$train_data_dir/train.$tgt_lang
+train_outfname_src=$train_processed_dir/train.SRC
+train_outfname_tgt=$train_processed_dir/train.TGT
 echo "Applying normalization and script conversion for train"
 input_size=`python preprocess_translate.py $train_infname_src $train_outfname_src $src_lang`
 input_size=`python preprocess_translate.py $train_outfname_src $train_outfname_tgt $tgt_lang`
 echo "Number of sentences in train: $input_size"
 
 # dev preprocessing
-$dev_infname_src=$devtest_data_dir/dev.$src_lang
-$dev_infname_tgt=$devtest_data_dir/dev.$tgt_lang
-$dev_outfname_src=$devtest_processed_dir/dev.SRC
-$dev_outfname_tgt=$devtest_processed_dir/dev.TGT
+dev_infname_src=$devtest_data_dir/dev.$src_lang
+dev_infname_tgt=$devtest_data_dir/dev.$tgt_lang
+dev_outfname_src=$devtest_processed_dir/dev.SRC
+dev_outfname_tgt=$devtest_processed_dir/dev.TGT
 echo "Applying normalization and script conversion for dev"
 input_size=`python preprocess_translate.py $dev_infname_src $dev_outfname_src $src_lang`
 input_size=`python preprocess_translate.py $dev_outfname_src $dev_outfname_tgt $tgt_lang`
 echo "Number of sentences in dev: $input_size"
 
 # test preprocessing
-$test_infname_src=$devtest_data_dir/test.$src_lang
-$test_infname_tgt=$devtest_data_dir/test.$tgt_lang
-$test_outfname_src=$devtest_processed_dir/test.SRC
-$test_outfname_tgt=$devtest_processed_dir/test.TGT
+test_infname_src=$devtest_data_dir/test.$src_lang
+test_infname_tgt=$devtest_data_dir/test.$tgt_lang
+test_outfname_src=$devtest_processed_dir/test.SRC
+test_outfname_tgt=$devtest_processed_dir/test.TGT
 echo "Applying normalization and script conversion for test"
 input_size=`python preprocess_translate.py $test_infname_src $test_outfname_src $src_lang`
 input_size=`python preprocess_translate.py $test_outfname_src $test_outfname_tgt $tgt_lang`
