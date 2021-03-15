1fa020512c5aa31d27845d23945034eaf600c518
Gowtham R <gowtham.ramesh1@gmail.com>
2021-03-15 21:56:03 +0530

    cleanline strip to be inline with subwordnmt

--- clean_vocab.py
+++ clean_vocab.py
@@ -1,17 +1,19 @@
 import sys
 
+
 def clean_vocab(in_vocab_fname, out_vocab_fname):
-    with open(in_vocab_fname,'r',encoding='utf-8') as infile, \
-         open(out_vocab_fname,'w',encoding='utf-8') as outfile:
+    with open(in_vocab_fname, "r", encoding="utf-8") as infile, open(
+        out_vocab_fname, "w", encoding="utf-8"
+    ) as outfile:
         for i, line in enumerate(infile):
-            fields=line.strip().split(' ')
-            if len(fields)==2:
+            fields = line.strip("\r\n ").split(" ")
+            if len(fields) == 2:
                 outfile.write(line)
-            if len(fields)!=2:
-                print('{}: {}'.format(i, line.strip()))
+            if len(fields) != 2:
+                print("{}: {}".format(i, line.strip()))
                 for c in line:
-                    print('{}:{}'.format(c,hex(ord(c))))
-                    
-if __name__ == '__main__':
-    clean_vocab(sys.argv[1],sys.argv[2])
-                
+                    print("{}:{}".format(c, hex(ord(c))))
+
+
+if __name__ == "__main__":
+    clean_vocab(sys.argv[1], sys.argv[2])
