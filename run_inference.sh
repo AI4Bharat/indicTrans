40d3218a7a8e7cd362a7ca0cb87d26161fac7d64
Gowtham R <gowtham.ramesh1@gmail.com>
2021-04-04 15:15:27 +0530

    add non-english-pair generation from anoop sir

--- run_inference.sh
+++ run_inference.sh
@@ -1,44 +1,51 @@
 src_lang=${1:-hi}
 tgt_lang=${2:-en}
 
-mkdir -p ../baselines
 expdir=../baselines/baselines-${src_lang}-${tgt_lang}
+mkdir -p $expdir
 
 if [[ -d $expdir ]]
 then
     echo "$expdir exists on your filesystem."
 else
     cd ../baselines
-	gsutil -m cp -r gs://mt-datasets/baseline_experiments/baselines-${src_lang}-${tgt_lang} .
+	gsutil -m cp -r gs://ai4b-anuvaad-nmt/baselines/transformer-base/baselines-${src_lang}-${tgt_lang}/vocab $expdir
+	gsutil -m cp -r gs://ai4b-anuvaad-nmt/baselines/transformer-base/baselines-${src_lang}-${tgt_lang}/final_bin $expdir
+	gsutil -m cp gs://ai4b-anuvaad-nmt/baselines/transformer-base/baselines-${src_lang}-${tgt_lang}/model/checkpoint_best.pt $expdir/model
 	cd ../indicTrans
 fi
 
 
-
-
-
 if [ $src_lang == 'hi' ] || [ $tgt_lang == 'hi' ]; then
-	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest anuvaad-legal tico19 sap-documentation-benchmark all)
+	#TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest anuvaad-legal tico19 sap-documentation-benchmark all)
+	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest)
 elif [ $src_lang == 'ta' ] || [ $tgt_lang == 'ta' ]; then
-	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest anuvaad-legal  tico19 all)
+	# TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest anuvaad-legal  tico19 all)
+	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest )
 elif [ $src_lang == 'bn' ] || [ $tgt_lang == 'bn' ]; then
-	TEST_SETS=( wat2021-devtest wat2020-devtest anuvaad-legal tico19 all)
+	# TEST_SETS=( wat2021-devtest wat2020-devtest anuvaad-legal tico19 all)
+	TEST_SETS=( wat2021-devtest wat2020-devtest )
 elif [ $src_lang == 'gu' ] || [ $tgt_lang == 'gu' ]; then
-	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest all)
+	# TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest all)
+	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest )
 elif [ $src_lang == 'as' ] || [ $tgt_lang == 'as' ]; then
 	TEST_SETS=( all )
 elif [ $src_lang == 'kn' ] || [ $tgt_lang == 'kn' ]; then
-	TEST_SETS=( wat2021-devtest anuvaad-legal all)
+	# TEST_SETS=( wat2021-devtest anuvaad-legal all)
+	TEST_SETS=( wat2021-devtest )
 elif [ $src_lang == 'ml' ] || [ $tgt_lang == 'ml' ]; then
-	TEST_SETS=( wat2021-devtest wat2020-devtest anuvaad-legal all)
+	# TEST_SETS=( wat2021-devtest wat2020-devtest anuvaad-legal all)
+	TEST_SETS=( wat2021-devtest wat2020-devtest )
 elif [ $src_lang == 'mr' ] || [ $tgt_lang == 'mr' ]; then
-	TEST_SETS=( wat2021-devtest wat2020-devtest all)
+	# TEST_SETS=( wat2021-devtest wat2020-devtest all)
+	TEST_SETS=( wat2021-devtest wat2020-devtest )
 elif [ $src_lang == 'or' ] || [ $tgt_lang == 'or' ]; then
 	TEST_SETS=( all )
 elif [ $src_lang == 'pa' ] || [ $tgt_lang == 'pa' ]; then
 	TEST_SETS=( all )
 elif [ $src_lang == 'te' ] || [ $tgt_lang == 'te' ]; then
-	TEST_SETS=( wat2021-devtest wat2020-devtest  anuvaad-legal all )
+	# TEST_SETS=( wat2021-devtest wat2020-devtest  anuvaad-legal all )
+	TEST_SETS=( wat2021-devtest wat2020-devtest  )
 fi
 
 if [ $src_lang == 'en' ]; then
@@ -65,3 +72,6 @@ for tset in ${TEST_SETS[@]};do
 	# for newline between different outputs
 	echo
 done
+
+gsutil -m cp -r $expdir/results gs://ai4b-anuvaad-nmt/baselines/transformer-base/baselines-${src_lang}-${tgt_lang}
+rm -r $expdir
