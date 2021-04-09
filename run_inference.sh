776215bb9cbc6b8db5a3ae7e146a2cf63fcd60fe
Sumanth Doddapaneni <dsumanth17@gmail.com>
2021-04-09 10:46:34 +0530

    update paths

--- run_inference.sh
+++ run_inference.sh
@@ -18,34 +18,34 @@ fi
 
 if [ $src_lang == 'hi' ] || [ $tgt_lang == 'hi' ]; then
 	#TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest anuvaad-legal tico19 sap-documentation-benchmark all)
-	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest)
+	TEST_SETS=( wat2021-devtest wat2020-devtest wat-2018 wmt-news )
 elif [ $src_lang == 'ta' ] || [ $tgt_lang == 'ta' ]; then
 	# TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest anuvaad-legal  tico19 all)
-	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest )
+	TEST_SETS=( wat2021-devtest wat2020-devtest wat-2018 wmt-news ufal-ta)
 elif [ $src_lang == 'bn' ] || [ $tgt_lang == 'bn' ]; then
 	# TEST_SETS=( wat2021-devtest wat2020-devtest anuvaad-legal tico19 all)
-	TEST_SETS=( wat2021-devtest wat2020-devtest )
+	TEST_SETS=( wat2021-devtest wat2020-devtest wat-2018)
 elif [ $src_lang == 'gu' ] || [ $tgt_lang == 'gu' ]; then
 	# TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest all)
-	TEST_SETS=( wmt-news wat2021-devtest wat2020-devtest )
+	TEST_SETS=( wat2021-devtest wat2020-devtest wmt-news )
 elif [ $src_lang == 'as' ] || [ $tgt_lang == 'as' ]; then
-	TEST_SETS=( all )
+	TEST_SETS=( pmi )
 elif [ $src_lang == 'kn' ] || [ $tgt_lang == 'kn' ]; then
 	# TEST_SETS=( wat2021-devtest anuvaad-legal all)
 	TEST_SETS=( wat2021-devtest )
 elif [ $src_lang == 'ml' ] || [ $tgt_lang == 'ml' ]; then
 	# TEST_SETS=( wat2021-devtest wat2020-devtest anuvaad-legal all)
-	TEST_SETS=( wat2021-devtest wat2020-devtest )
+	TEST_SETS=( wat2021-devtest wat2020-devtest wat-2018)
 elif [ $src_lang == 'mr' ] || [ $tgt_lang == 'mr' ]; then
 	# TEST_SETS=( wat2021-devtest wat2020-devtest all)
 	TEST_SETS=( wat2021-devtest wat2020-devtest )
 elif [ $src_lang == 'or' ] || [ $tgt_lang == 'or' ]; then
-	TEST_SETS=( all )
+	TEST_SETS=( wat2021-devtest )
 elif [ $src_lang == 'pa' ] || [ $tgt_lang == 'pa' ]; then
-	TEST_SETS=( all )
+	TEST_SETS=( wat2021-devtest )
 elif [ $src_lang == 'te' ] || [ $tgt_lang == 'te' ]; then
 	# TEST_SETS=( wat2021-devtest wat2020-devtest  anuvaad-legal all )
-	TEST_SETS=( wat2021-devtest wat2020-devtest  )
+	TEST_SETS=( wat2021-devtest wat2020-devtest wat-2018)
 fi
 
 if [ $src_lang == 'en' ]; then
@@ -58,11 +58,11 @@ fi
 for tset in ${TEST_SETS[@]};do
 	echo $tset $src_lang $tgt_lang
 	if [ $tset == 'wat2021-devtest' ]; then
-		SRC_FILE=${expdir}/devtest/$tset/test.$src_lang
-		REF_FILE=${expdir}/devtest/$tset/test.$tgt_lang
+		SRC_FILE=${expdir}/benchmarks/$tset/test.$src_lang
+		REF_FILE=${expdir}/benchmarks/$tset/test.$tgt_lang
 	else
-		SRC_FILE=${expdir}/devtest/$tset/en-${indic_lang}/test.$src_lang
-		REF_FILE=${expdir}/devtest/$tset/en-${indic_lang}/test.$tgt_lang
+		SRC_FILE=${expdir}/benchmarks/$tset/en-${indic_lang}/test.$src_lang
+		REF_FILE=${expdir}/benchmarks/$tset/en-${indic_lang}/test.$tgt_lang
 	fi
 	RESULTS_DIR=${expdir}/results/$tset
 
